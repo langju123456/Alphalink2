34564b705d4c254a3018095aa45ddfca83b072c6
Ju Lang <langju123456@gmail.com>
2026-02-21 01:46:18 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7371045752-44c9d",
+  "appId": "1:904745204533:web:884560d11edce1e6c12a7c",
+  "apiKey": "AIzaSyDnNk0N3kpuR9O-arNVhPdwF40c7J6FhzI",
+  "authDomain": "studio-7371045752-44c9d.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "904745204533"
+};
